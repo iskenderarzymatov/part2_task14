39695d001a79c70085fae45cfdd6df9f3d1cae78
iskenderarzymatov <iskenderarzymatov@gmail.com>
2019-11-21 10:22:48 -0500

    part2_task14

---
+++ task14.py
@@ -0,0 +1,6 @@
+numbers = [int(number) for number in input('Введите числа через пробел: ').split()] #генератор списка
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+    else:
+        continue
